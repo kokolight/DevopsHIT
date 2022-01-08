2e2c5d0a042de105f8e2afa6b17822861702b605
kokolight <tsegal97@gmail.com>
2022-01-08 15:47:16 +0200

    added scripts directory

---
+++ Scripts/Site Monitoring/application_monitor.ps1
@@ -0,0 +1,17 @@
+﻿$hostname = $args[0]
+$port = $args[1]
+if(!$hostname -or !$port)
+{
+    exit 1
+}
+
+$isAvailable = Test-NetConnection -ComputerName $hostname -Port $port
+
+if($isAvailable.TcpTestSucceeded)
+{
+    Write-Host "Succeeded"
+    exit 0
+}
+
+Write-Host "Failed"
+exit 1
